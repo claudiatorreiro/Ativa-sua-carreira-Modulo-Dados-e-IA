5251d2b328427ee7d853a6f2fc6f1ef02f012741
Claudia Torreiro <143898254+claudiatorreiro@users.noreply.github.com>
2026-04-30 21:10:45 -0300

    Atualização music_genre no SQL

--- sql/07_tratamento_categoricos.sql
+++ sql/07_tratamento_categoricos.sql
@@ -1,9 +1,14 @@
+-- Projeto: Análise de sucesso de músicas no Spotify
+-- Etapa: Tratamento de valores atípicos categóricos
+-- Objetivo: Padronizar inconsistências na variável main_music_genre
+
 CREATE OR REPLACE TABLE `projeto-01-rota-b-claudia.dados_tratados.musicas_spotify_categorico_tratado` AS
 SELECT
   *,
   CASE
-    WHEN main_music_genre IS NULL OR main_music_genre = '' THEN 'Nao informado'
-    WHEN main_music_genre = 'Disco-pop' THEN 'Disco pop'
-    ELSE main_music_genre
+    WHEN main_music_genre IS NULL OR TRIM(main_music_genre) = '' THEN 'Nao informado'
+    WHEN LOWER(TRIM(main_music_genre)) = 'main genre' THEN 'Nao informado'
+    WHEN LOWER(TRIM(main_music_genre)) = 'disco-pop' THEN 'Disco pop'
+    ELSE TRIM(main_music_genre)
   END AS main_music_genre_tratado
 FROM `projeto-01-rota-b-claudia.dados_tratados.musicas_spotify_sem_duplicados`;
