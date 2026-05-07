11c4b4d6f9beb06afd55cfbf8e5c2fc2792ce584
Claudia Torreiro <143898254+claudiatorreiro@users.noreply.github.com>
2026-05-07 10:56:02 -0300

    Atualização do sql referente ao tratamento de valores numéricos

--- sql/08_tratamento_numericos.sql
+++ sql/08_tratamento_numericos.sql
@@ -1,18 +1,25 @@
+-- Projeto: Análise de sucesso de músicas no Spotify
+-- Etapa: Tratamento de valores numéricos
+-- Objetivo: Padronizar tipos numéricos e preparar a tabela final do Spotify
+
 CREATE OR REPLACE TABLE `projeto-01-rota-b-claudia.dados_tratados.musicas_spotify_final` AS
+
 SELECT
-  track_id,
-  track_name,
-  artists_name,
-  artist_count,
-  main_music_genre_tratado,
-  main_country,
-  released_year,
-  released_month,
-  released_day,
 
-  SAFE_CAST(in_spotify_playlists AS INT64) AS in_spotify_playlists,
+    track_id,
+    track_name,
+    artists_name,
+    artist_count,
+    main_music_genre_tratado,
+    main_country,
+    released_year,
+    released_month,
+    released_day,
 
-  in_spotify_charts,
-  streams
+    SAFE_CAST(in_spotify_playlists AS INT64) AS in_spotify_playlists,
+
+    in_spotify_charts,
+
+    streams
 
 FROM `projeto-01-rota-b-claudia.dados_tratados.musicas_spotify_categorico_tratado`;
