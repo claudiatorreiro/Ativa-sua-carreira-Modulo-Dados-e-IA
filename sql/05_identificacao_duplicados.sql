e443daad722c01d7579c3daa7697afc7e55e7a98
Claudia Torreiro <143898254+claudiatorreiro@users.noreply.github.com>
2026-05-11 11:39:22 -0300

    Atualização da  identificatição dos dados duplicados
    Added checks for duplicate records in competitor music data.

--- sql/05_identificacao_duplicados.sql
+++ sql/05_identificacao_duplicados.sql
@@ -1,18 +1,40 @@
 -- Projeto: Análise de sucesso de músicas no Spotify
 -- Etapa: Identificação de registros duplicados
--- Objetivo: Verificar duplicidades na base de músicas do Spotify
+-- Objetivo: Verificar duplicidades nas bases Spotify e concorrentes
+
+-- Verificação de duplicidades Spotify
 
 SELECT
-  track_id,
-  COUNT(*) AS quantidade
+    track_id,
+    COUNT(*) AS quantidade
+
 FROM `projeto-01-rota-b-claudia.dados_tratados.musicas_spotify_tratada`
+
 GROUP BY track_id
+
 HAVING COUNT(*) > 1;
 
+-- Verificação de duplicidades por música e artista
+
 SELECT
-  track_name,
-  artists_name,
-  COUNT(*) AS quantidade
+    track_name,
+    artists_name,
+    COUNT(*) AS quantidade
+
 FROM `projeto-01-rota-b-claudia.dados_tratados.musicas_spotify_tratada`
+
 GROUP BY track_name, artists_name
+
+HAVING COUNT(*) > 1;
+
+-- Verificação de duplicidades concorrentes
+
+SELECT
+    track_id,
+    COUNT(*) AS quantidade
+
+FROM `projeto-01-rota-b-claudia.dados_tratados.musicas_concorrentes_tratada`
+
+GROUP BY track_id
+
 HAVING COUNT(*) > 1;
