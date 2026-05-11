ad541abb4767fc2725dc7f49c775ffb6fd5fe902
Claudia Torreiro <143898254+claudiatorreiro@users.noreply.github.com>
2026-05-11 11:37:33 -0300

    Refeito o treatamento lógico no SQL main_music_genre

--- sql/07_tratamento_categoricos.sql
+++ sql/07_tratamento_categoricos.sql
@@ -3,12 +3,16 @@
 -- Objetivo: Padronizar inconsistências na variável main_music_genre
 
 CREATE OR REPLACE TABLE `projeto-01-rota-b-claudia.dados_tratados.musicas_spotify_categorico_tratado` AS
+
 SELECT
-  *,
-  CASE
-    WHEN main_music_genre IS NULL OR TRIM(main_music_genre) = '' THEN 'Nao informado'
-    WHEN LOWER(TRIM(main_music_genre)) = 'main genre' THEN 'Nao informado'
-    WHEN LOWER(TRIM(main_music_genre)) = 'disco-pop' THEN 'Disco pop'
-    ELSE TRIM(main_music_genre)
-  END AS main_music_genre_tratado
+    *,
+
+    CASE
+        WHEN track_name = 'Style' THEN 'Pop'
+        WHEN main_music_genre IS NULL OR TRIM(main_music_genre) = '' THEN 'Nao informado'
+        WHEN LOWER(TRIM(main_music_genre)) = 'main genre' THEN 'Nao informado'
+        WHEN LOWER(TRIM(main_music_genre)) = 'disco-pop' THEN 'Disco pop'
+        ELSE TRIM(main_music_genre)
+    END AS main_music_genre_tratado
+
 FROM `projeto-01-rota-b-claudia.dados_tratados.musicas_spotify_sem_duplicados`;
